81c84430c1cba9f67d4835c56726766fe2542927
Nathan <nldblanch@gmail.com>
2025-02-04 09:14:24 +0000

    update image styling for profile

--- src/app/_components/soft-item-card.tsx
+++ src/app/_components/soft-item-card.tsx
@@ -19,8 +19,8 @@ export const SoftItemCard = ({
           src={item.photo_source[0] ?? ""}
           alt={item.name}
           className="aspect-square rounded-lg object-cover"
-          width="20"
-          height="20"
+          width="500"
+          height="500"
         />
         <h3 className="px-2 pt-2">{capitaliseFirstLetters(item.name)}</h3>
         <p className="px-2 font-semibold">{convertPennyToPounds(item.price)}</p>
